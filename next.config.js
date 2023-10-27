11a8fbe618685943374d571f92791ebacc986cf4
JoyChanya <chanya.joy23@gmail.com>
2023-10-27 23:50:34 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true,
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/home',
+          destination: '/',
+          permanent: true,
+        },
+      ]
     },
-};
-
-module.exports = nextConfig;
+  }
